873e0a4d1d8e55023349c27a7297aacab1dadd79
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2019-03-11 17:26:58 +0200

    added some styles to project status

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -16,8 +16,9 @@
             <div>12th Jan 2019</div>
           </v-flex>
           <v-flex xs12 sm4 md2>
-            <div class="caption grey--text">{{project.status}}</div>
-            <div>going...</div>
+            <div class="right">
+              <v-chip small :class="`${project.status} white--text caption my-2`">{{project.status}}</v-chip>
+            </div>
           </v-flex>
         </v-layout>
         <v-divider></v-divider>
@@ -78,6 +79,15 @@ export default {
 }
 .project.overdue {
   border-left: 4px solid tomato;
+}
+.v-chip.complete {
+  background: #3cd1c2;
+}
+.v-chip.ongoing {
+  background: orange;
+}
+.v-chip.overdue {
+  background: tomato;
 }
 </style>
 
